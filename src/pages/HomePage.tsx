d2fa592514791746c40089500705d574ba7a390b
unknown <nikakotov2005@gmail.com>
2024-12-27 20:14:11 +0200

    set base routing

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+   return (
+      <section className="p-9">
+         <h2>Home page</h2>
+      </section>
+   );
+}
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+   return (
+      <section className="p-9">
+         <h2>Login page</h2>
+      </section>
+   );
+}
