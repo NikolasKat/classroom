fa8efd0ee931910d74f90382ed7538ccc384c4f3
unknown <nikakotov2005@gmail.com>
2024-12-29 21:40:19 +0200

    create card component and alert component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,21 @@
+import SubjectCard from "../components/SubjectCard";
+
 export default function HomePage() {
-   return (
-      <section className="p-9">
-         <h2>Home page</h2>
-      </section>
-   );
+    return (
+        <section className="p-9 flex justify-between flex-wrap">
+            {/* <h2>Home page</h2> */}
+            <SubjectCard
+                teacherName="Мария Семёновна"
+                subjectName="Математика"
+                bgImg="pink"
+                color="red"
+            />
+            <SubjectCard
+                teacherName="Зинаида Борисовна"
+                subjectName="История Украины"
+                bgImg="cyan"
+                color="purple"
+            />
+        </section>
+    );
 }
