44393cac725aa4537dfb68ed84f715ee37383eee
unknown <nikakotov2005@gmail.com>
2024-12-29 21:46:26 +0200

    clean unnecessary stuff

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,21 +1,7 @@
-import SubjectCard from "../components/SubjectCard";
-
 export default function HomePage() {
     return (
         <section className="p-9 flex justify-between flex-wrap">
-            {/* <h2>Home page</h2> */}
-            <SubjectCard
-                teacherName="Мария Семёновна"
-                subjectName="Математика"
-                bgImg="pink"
-                color="red"
-            />
-            <SubjectCard
-                teacherName="Зинаида Борисовна"
-                subjectName="История Украины"
-                bgImg="cyan"
-                color="purple"
-            />
+            <h2>Home page</h2>
         </section>
     );
 }
