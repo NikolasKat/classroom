06d4adbdee1c42bd606c2a7d928d7ed915e05ede
unknown <nikakotov2005@gmail.com>
2025-01-02 21:30:53 +0200

    add useNavigate from register page to login page and home page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,5 @@
-import { useForm, SubmitHandler } from 'react-hook-form';
-import { Link } from 'react-router-dom';
+import { useForm, SubmitHandler } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
 
 interface UserData {
     email: string;
@@ -13,7 +13,12 @@ const LoginPage = () => {
         formState: { errors },
     } = useForm<UserData>();
 
-    const onSubmit: SubmitHandler<UserData> = (data) => console.log(data);
+    const navigate = useNavigate();
+
+    const onSubmit: SubmitHandler<UserData> = (data) => {
+        console.log(data);
+        navigate("/");
+    };
 
     return (
         <div className="pt-64">
@@ -33,9 +38,9 @@ const LoginPage = () => {
                         id="email"
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         placeholder="name@flowbite.com"
-                        {...register('email', { required: true })}
+                        {...register("email", { required: true })}
                     />
-                    {errors.email?.type === 'required' && (
+                    {errors.email?.type === "required" && (
                         <p role="alert">Email is required</p>
                     )}
                 </div>
@@ -50,27 +55,11 @@ const LoginPage = () => {
                         type="password"
                         id="password"
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        {...register('password', { required: true })}
+                        {...register("password", { required: true })}
                     />
-                    {errors.password?.type === 'required' && (
+                    {errors.password?.type === "required" && (
                         <p role="alert">Password is required</p>
                     )}
-                </div>
-                <div className="flex items-start mb-5">
-                    <div className="flex items-center h-5">
-                        <input
-                            id="remember"
-                            type="checkbox"
-                            value=""
-                            className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                        />
-                    </div>
-                    <label
-                        htmlFor="remember"
-                        className="ms-2 text-lg font-medium text-gray-900 dark:text-gray-300"
-                    >
-                        Remember me
-                    </label>
                 </div>
                 <button
                     type="submit"
@@ -80,10 +69,10 @@ const LoginPage = () => {
                 </button>
             </form>
             <h3 className="text-xl mt-5 text-center">
-                or{' '}
+                or{" "}
                 <Link to="/register" className="font-semibold ">
                     Register
-                </Link>{' '}
+                </Link>{" "}
                 if you don`t have an account
             </h3>
         </div>
