d9181756d90f3321f3e659b43f57ceefad667edd
unknown <nikakotov2005@gmail.com>
2025-01-03 19:42:40 +0200

    create base imports and usage

---
+++ server/server.js
@@ -0,0 +1,25 @@
+require("dotenv").config(); // подключаем файл конфигурации
+
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const PORT = process.env.PORT || 4444; // достаём и присваиваем порт из файла конфигурации
+const app = express(); // создаём серверное приложение
+
+app.get("/", (req, res) => {
+   res.status(200).json("Server started!");
+});
+
+const start = async () => {
+   try {
+      app.listen(PORT, () => {
+         // запуск приложения
+         console.log(`SERVER STARTED ON PORT = ${PORT} !`);
+      });
+   } catch (error) {
+      console.log(error);
+   }
+};
+
+start();
